d89bafd51c0c19946a11c7878f5f38a8d15cf360
Max Volz <maxwellvolz@gmail.com>
2018-05-07 22:27:43 -0700

    bonfire code; skeeball setup; need to test chase cam

--- src/scenes/skeeball.js
+++ src/scenes/skeeball.js
@@ -9,6 +9,8 @@ import * as UTILS from '../component/utils';
 import * as MtnModels from '../3dcomponents/mtnModels';
 import * as MtnLights from '../3dcomponents/mtnLights';
 
+import * as SkeeballModels from '../3dcomponents/skeeballModels';
+
 ObjLoader(THREE);
 
 let app = {}
@@ -38,10 +40,83 @@ export default class Skeeball extends Component {
 	PlaySkeeball() {
 		
 		// init app
-		
+	const mouse = new WHS.VirtualMouseModule();
+		const startPos = new THREE.Vector3(20,10,0);
+
+		camera = new WHS.DefineModule('camera', new WHS.PerspectiveCamera({
+			fov: 100,
+			position: {
+				x: 0,
+				y: 40,
+				z: 60
+			}
+		}));
+
+		app = new WHS.App([
+			new WHS.ElementModule(document.getElementById('whs2')),
+			new WHS.SceneModule(),
+			camera,
+			new WHS.RenderingModule({
+				bgColor: 0x162129,
+				renderer: {
+					antialias: true,
+					shadowmap: {
+						type: THREE.PCFShadowMap
+					}
+				}
+			}, { shadow: true }),
+			new PHYSICS.WorldModule({
+				ammo: 'https://cdn.rawgit.com/WhitestormJS/physics-module-ammonext/7a25628e/vendor/ammo.js'
+			}),
+			new WHS.ResizeModule(),
+			mouse,
+			orbitControls
+		]);
+				
 		// create static models
 
-		// create balls
+		const skeeballMachine = SkeeballModels.Machine();
+
+		skeeballMachine.addTo(app);
+
+		// create balls in slot
+		// ref them in order
+		// on click of area 
+		
+		// shoot ball
+		const shootBall = (shotPosition, shotPower, shotAngle) => {
+			this.props.ballWasMade("Shot fired!");
+			let skeeballball = new WHS.Sphere({
+				geometry: {
+					radius: 1,
+					widthSegments: 32,
+					heightSegments: 32
+				},
+				modules: [
+					new PHYSICS.SphereModule({
+						mass: 20
+					}),
+				material: new THREE.MeshPhongMaterial({
+					color: 0xFFFF00
+				}),
+				position: shotPosition
+			});
+			skeeballball.addTo(app);
+			
+			skeeballball.use('physics').setLinearVelocity({
+				x: shotPower,
+				y: shotPower / 10,
+				z: shotAngle
+			});
+
+			// TEST
+			// not sure if camera will need to be removed from previous ball
+			// see: https://whs.io/module-core.Component.html
+			// need 'chase' camera positioning;
+			camera.addTo(skeeballball);
+
+		};  			
+
 
 		// click listeners for balls 
 	
@@ -56,9 +131,22 @@ export default class Skeeball extends Component {
 		// 2 player keyboard mode
 		
 		// loops
+		let cycle = 3;
+		
+		const loop = new WHS.Loop((clock) => {
+			if (clock.getElapsedTime() > cycle){	
+				// shoot random ball
+				shootBall(startPos, 20, 5);
+				cycle += 3.2;
+			}
+		});
+		
+		loop.start(app);
 
 		// app start
-	}
+		app.start();		
+	}	
+	
 	
 	render() {
 		return(<span></span>);
